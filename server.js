98d3a6ca798a3df307d9c23a18802c1e35dc99f9
trobol <thorfernbacher@gmail.com>
2019-09-04 18:44:29 -0400

    Delete server.js

--- server.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const app = express();
-const port = 80;
-
-app.use(express.static(__dirname));
-
-app.listen(port, () => console.log(`Server listening on port ${port}!`))
